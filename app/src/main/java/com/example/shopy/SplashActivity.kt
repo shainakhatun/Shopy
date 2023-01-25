bf6844dbac49b04dba1be59cce1407c918dd9330
Shaina <shainakhatun21@navgurukul.org>
2023-01-25 18:02:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shopy/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.shopy
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var image:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        image=findViewById(R.id.img)
+        image.alpha=0f
+        image.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i= Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
+
+    }
+}
