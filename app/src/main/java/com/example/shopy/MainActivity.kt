31f2d5a66e181bd114646db24a3bbbf9d9233e3d
Shaina <shainakhatun21@navgurukul.org>
2023-01-27 12:26:34 +0530

    menubar completed

--- app/src/main/java/com/example/shopy/MainActivity.kt
+++ app/src/main/java/com/example/shopy/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.fragment_home)
+        setContentView(R.layout.fragment_menu)
+        setContentView(R.layout.fragment_person)
+        setContentView(R.layout.fragment_add_to_card)
+
     }
 }
